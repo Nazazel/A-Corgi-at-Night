5c0e819818a34437b98ade897f96497a2c4d05b6
CodiakVII <codiak759@gmail.com>
2017-11-16 01:30:57 -0800

    A Wild Teleport Script Appeared!

--- Corgi at Night/Assets/Teleport.cs
+++ Corgi at Night/Assets/Teleport.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour {
 
+	public GameObject teleportExit;
+	private bool teleportable;
+	private Doggo player;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.Find ("QuinSpriteFinal_1").GetComponent<Doggo> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.R) && !player.jumping && !player.barking && !player.crouching) {
+			
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.CompareTag ("Player")) {
+			teleportable = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		if (coll.gameObject.CompareTag ("Player")) {
+			teleportable = false;
+		}
 	}
 }
