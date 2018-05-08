9a6387673a44e75dc3e2feda47997e9d0b4b2a1c
CodiakVII <codiak759@gmail.com>
2018-05-08 16:09:12 -0700

    Progressing in Menu Animations

--- Corgi at Night/Assets/Scripts/Pibble.cs
+++ Corgi at Night/Assets/Scripts/Pibble.cs
@@ -624,9 +624,9 @@ void OnCollisionExit2D(Collision2D colll)
 
     public IEnumerator intro()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(3.5f);
         pa.Play("LookR");
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(3.0f);
         pa.Play("SniffStand");
         QuinnAS.clip = sniffAudio;
         QuinnAS.Play();
