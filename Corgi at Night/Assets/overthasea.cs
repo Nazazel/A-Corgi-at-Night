65a9c2b02d38ff7a7588dee81bd65d4875f1ee58
CodiakVII <codiak759@gmail.com>
2018-10-15 20:18:57 -0700

    A couple bug fixes

---
+++ Corgi at Night/Assets/overthasea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overthasea : MonoBehaviour {
+
+    public GameObject bgc;
+
+    // Use this for initialization
+    void Start()
+    {
+        bgc = GameObject.Find("BGController");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            bgc.GetComponent<BGController>().FadeIn(1.0f);
+            Destroy(gameObject);
+        }
+    }
+}
